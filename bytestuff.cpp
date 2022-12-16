847e2ec49f411d7e4f4a82fa8f3b2ac6cc2803fc
Tejeswara Murthy palwadi <tejeswarmurthypalwadi@gmail.com>
2022-12-16 14:01:10 +0530

    Update bytestuff.cpp

--- bytestuff.cpp
+++ bytestuff.cpp
@@ -1,44 +1,43 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-void bytestuffing()
+string bytestuffing()
 {
-    string  s1;
-    cout<<"Enter the Message to be send:";
-    cin >> s1;
-    s1.insert(0,"F");
-    for(int i=1;i<s1.size();i++)
+    string s;
+    cout<<"Enter string to Bytestuff:";
+    cin >> s;
+    s.insert(0,"F");
+    for(int i=0;i<s.length();i++)
     {
-        if(s1.substr(i,3)=="ESC"||s1.substr(i,4)=="FLAG")
-        {
-            s1.insert(i,"ESC");
+        if(s.substr(i,3)=="ESC"||s.substr(i,4)=="FLAG"){
+            s.insert(i,"ESC");
             i+=3;
         }
     }
-    s1.insert(s1.size(),"F");
-    cout<<"Byte Stuffed String : "<<s1<<endl;
+    s+="F";
+    cout<<"Bytestuffed String is : "<<s<<endl;
+    return s;
 }
-void bytedestuffing()
+
+void bytedestuffing(string s)
 {
-    string s;
-    cout<<"Enter The Byte Stuffed String:";
-    cin >> s;
     s.erase(0,1);
     s.erase(s.size()-1,1);
-    for(int i=0;i<s.length()-1;i++)
+
+    for(int i=0;i<s.length();i++)
     {
-        if(s.substr(i,3)=="ESC" && (s.substr(i+3,3)=="ESC"||s.substr(i+3,4)=="FLAG"))
+        if(s.substr(i,3)=="ESC"&&(s.substr(i+3,4)=="FLAG"||s.substr(i+3,3)=="ESC"))
         {
             s.erase(i,3);
         }
     }
     cout<<"Byte Destuffed String : "<<s<<endl;
-
 }
+
 int main()
 {
- bytestuffing();
- bytedestuffing();
-
+    bytedestuffing(bytestuffing());
+    
+    return 0;
 }
